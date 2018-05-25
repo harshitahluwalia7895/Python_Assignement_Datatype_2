f11f96fac7bcd227b331cec6bf88284c2c21d56d
harshitahluwalia7895 <harshit.ahluwalia7895889924@gmail.com>
2018-05-25 20:09:01 +0530

    final Commit

---
+++ answer_2.py
@@ -0,0 +1,4 @@
+tup=('Apple','Samsung','Nokia','Asus','Micromax')
+print(tup)
+print("Largest element of the above tuple is:%s" % (max(tup)))
+print("Smallest element of the above tuple is:%s" % (min(tup)))
